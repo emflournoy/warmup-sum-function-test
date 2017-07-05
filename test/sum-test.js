10533ed29e124eb53b42d11ada2092f135c17a21
Elizabeth Flournoy <bflournoy@gmail.com>
2017-07-05 13:53:02 -0600

    five tests

--- test/sum-test.js
+++ test/sum-test.js
@@ -14,5 +14,23 @@ describe ('Sum', function() {
       expect(sum()).to.equal(0);
       assert.equal(sum(),0);
     });
-    
+
+    it('Should return one number as a result', function(){
+      var testAns = sum(1,5,6,8)
+      testAns.should.have.lengthOf(1);
+      expect(sum(1,3)).to.be.a('number');
+      expect(sum(1,3)).to.equal(4);
+    });
+
+    it('Should add numbers', function(){
+      assert.equal(sum(1,2,3),6);
+      assert.equal(sum(3,-1,2),4);
+      assert.equal(sum(0,3,4),7);
+    })
+
+    it('Should return "error: invalid character" if you pass in letters', function(){
+      expect(sum(1,'g')).to.equal('error: invalid character');
+    })
+
+
 });
